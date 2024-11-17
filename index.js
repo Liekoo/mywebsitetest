627019b0eed9092f79d5fdf15062f8ab1333cd35
Lyco <151441074+Liekoo@users.noreply.github.com>
2024-11-17 10:08:14 +0800

    Add files via upload
    Initial Release

---
+++ index.js
@@ -0,0 +1,55 @@
+/* console.log is a way of showing output 
+// and quatation mark is a string character 
+console.log("Hello World Panget");
+#########################################################################
+*/
+
+/* let, const as var
+
+let firstname = "budo"
+let lastname = "pogi"
+console.log(firstname + lastname) = "Budopogi" 
+
+const interestRate = 1.2;
+const interesRate = 1.2;
+console.log(firstname + lastname)  result = 
+########################################################################
+let name = 'Lyco'; string
+let age = 18; // int
+let badingsilyco = false; //boolean T|F
+let firstName = undefined;
+let selectedColor = null;
+
+#######################################################################
+
+const numbers = [1, 2, 3];
+const unshifted = numbers.unshift(5)
+console.log(numbers); result would be = [ 5, 1, 2, 3 ]
+ pushing or inserting in array
+ ###################################################################
+  if else statement 101 
+  if (condition1) {
+    // block of code to be executed if the condition is true
+} else if () {
+    }
+} else(condition2) { 
+  // block of code to be executed if the condition is false
+ } 
+#####################################################################
+WHILE STATEMENT EXAMPLE
+let count = 1;  // Initialize the count variable
+
+while (count <= 5) {  // Condition to check if count is "<" less than to 5
+    console.log(count);  // Print the current value of count
+    count++;  // Increment count by 1
+}
+###################################################################
+/*
+METHODS
+
+.push
+.call
+.shift
+.unshifted
+.pop
+*/
